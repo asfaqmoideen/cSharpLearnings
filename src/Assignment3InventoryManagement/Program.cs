985f330010ee8f27ed545f4d323fac2a6e7148e9
asfaqMoideenSoliton <asfaqmoideen.askar@solitontech.com>
2024-01-12 19:09:47 +0530

    fix: blankspaces and comments

--- src/Assignment3InventoryManagement/Program.cs
+++ src/Assignment3InventoryManagement/Program.cs
@@ -6,11 +6,12 @@ namespace Assignments
 /// Initialies the program execution
 /// </summary>
     internal partial class Program
-    { 
-    /// <summary>
-    /// The is the list of products stored as list of objects
-    /// </summary>
+    {
+        /// <summary>
+        /// The is the list of products stored as list of objects
+        /// </summary>
         private static List<Product> _productList = new List<Product>();
+
         /// <summary>
         /// Method to get Product Name
         /// </summary>
@@ -21,6 +22,7 @@ namespace Assignments
             string productName = Console.ReadLine();
             return (productName != null) ? productName : "-";
         }
+
         /// <summary>
         /// Method to get product ID
         /// </summary>
@@ -31,6 +33,7 @@ namespace Assignments
             string productID = Console.ReadLine();
             return (productID != null) ? productID : "_";
         }
+
         /// <summary>
         /// Method to get product Price
         /// </summary>
@@ -49,6 +52,7 @@ namespace Assignments
             while (isPriceDouble != true);
             return productPrice;
         }
+
         /// <summary>
         /// Function to get product quantity
         /// </summary>
@@ -64,7 +68,7 @@ namespace Assignments
                 isQuantityDouble = int.TryParse(tempQuan, out productQuantity);
             }
             while (isQuantityDouble != true);
-            return (productQuantity);
+            return productQuantity;
         }
 
         private static void Main()
@@ -140,6 +144,7 @@ namespace Assignments
                 Console.WriteLine("Redirecing to Menu");
             }
         }
+
         private static void ViewProducts()
         {
             if (_productList.Count > 0)
@@ -156,6 +161,7 @@ namespace Assignments
                 NoProductsYet();
             }
         }
+
         private static void SearchProducts()
         {
             if (_productList.Count > 0)
@@ -176,7 +182,8 @@ namespace Assignments
             {
                 NoProductsYet();
             }
-        } 
+        }
+
         private static void DeleteProduct()
         {
             if (_productList.Count > 0)
@@ -213,6 +220,7 @@ namespace Assignments
                 NoProductsYet();
             }
         }
+
         private static void EditProduct()
         {
             Product editproduct;
@@ -259,6 +267,7 @@ namespace Assignments
                 NoProductsYet();
             }
         }
+
         /// <summary>
         /// Prints no products if the list is empty
         /// </summary>
