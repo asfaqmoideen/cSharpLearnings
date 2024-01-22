e64298695ea3c664e476ad7d887836892ffa2ed6
asfaqMoideenSoliton <asfaqmoideen.askar@solitontech.com>
2024-01-22 19:27:16 +0530

    fix: Unhandled Exception with KeyNotFoundException

--- src/ErrorHandling/Program.cs
+++ src/ErrorHandling/Program.cs
@@ -7,6 +7,7 @@
     {
         private static void Main()
         {
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(MyHandler);
             while (true)
             {
                 Tasks tasks = new Tasks();
@@ -14,7 +15,17 @@
                 tasks.TryCatchFinallyDivideByZeroExeption();
                 tasks.TryCatchIndexOutsideBoundary();
                 tasks.InvalidUserInputException();
+                tasks.KeyNotFoundException();
             }
         }
+
+        private static void MyHandler(object sender, UnhandledExceptionEventArgs args)
+        {
+            Exception e = (Exception)args.ExceptionObject;
+            Console.WriteLine("Unhandled Exception: "+ e.Message);
+            Console.WriteLine("RunTime Terminating: "+ args.IsTerminating );
+            Console.WriteLine("The StackTrace is :" +e.StackTrace);
+            Console.ReadKey();
+        }
     }
-}
+} 
--- src/ErrorHandling/Tasks.cs
+++ src/ErrorHandling/Tasks.cs
@@ -1,4 +1,6 @@
-﻿namespace Assignments
+﻿using System.Formats.Asn1;
+
+namespace Assignments
 {
     /// <summary>
     /// Class with all the tasks
@@ -10,41 +12,34 @@
         /// </summary>
         public void TryCatchFinallyDivideByZeroExeption()
         {
-            Console.WriteLine("Divide By Zero Exception");
+            Console.WriteLine("Task 1 - Divide By Zero Exception");
             Console.WriteLine("Enter a Divident");
             int divident = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter a Divider");
-            int divider = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a Divisor");
+            int divisor = int.Parse(Console.ReadLine());
             try
             {
-                decimal result = divident / divider;
-                int[] myNumbers = { 1, 2, 3 };
-                Console.WriteLine(myNumbers[10]);
+                decimal result = divident / divisor;
             }
             catch (DivideByZeroException e1)
             {
-               Console.WriteLine(e1.Message);
-            }
-            catch (IndexOutOfRangeException e2)
-            {
-                Console.WriteLine(e2.Message);
+                Console.WriteLine(e1.Message);
             }
             finally
             {
-                Console.WriteLine("Block Executed\n");
+                Console.WriteLine("Task 1 Executed\n");
             }
         }
 
         /// <summary>
-        /// Try catch finally block to catch the outside boundary exception
+        /// Try catch finally block to catch the exception thrown from the inner function call without catch exception
         /// </summary>
         public void TryCatchIndexOutsideBoundary()
         {
-            Console.WriteLine("Index Outside Boundary Exception");
+            Console.WriteLine("Task 2 Index Outside Boundary Exception");
             try
             {
-                int[] myNumbers = { 1, 2, 3 };
-                Console.WriteLine(myNumbers[10]);
+                TryCatchIndexOutsideBoundary1();
             }
             catch (IndexOutOfRangeException e2)
             {
@@ -52,29 +47,69 @@
             }
             finally
             {
-                Console.WriteLine("Block Executed\n");
+                Console.WriteLine("Task 2  Executed\n");
             }
-        }
+        } 
         /// <summary>
-        /// Method to try catch InvalidUserInputException
+        /// Just throws an Exception will be catch by enclosing scope of function
         /// </summary>
-        public void InvalidUserInputException()
+        public void TryCatchIndexOutsideBoundary1()
         {
             try
             {
-                uint ageToVote = uint.Parse(Console.ReadLine());
+                int[] myNumbers = { 1, 2, 3 };
+                Console.WriteLine(myNumbers[10]);
+            }
+            catch (IndexOutOfRangeException e2)
+            {
+                throw new IndexOutOfRangeException("The given index is outside the boundary");
+            }
+        }
+        /// <summary>
+        /// Method to try catch with custom Exception InvalidAgeInputException
+        /// </summary>
+        public void InvalidUserInputException()
+        {
+            Console.WriteLine("Task 3 Invalid User Input With Custom Exception Class");
+            try
+            {
+                // uint ageToVote = uint.Parse(Console.ReadLine());
+                uint ageToVote = 2;
                 if (ageToVote < 18 )
                 {
                     throw new InvalidAgeInputException();
                 }
             }
-            catch (InvalidAgeInputException e2)
+            catch (InvalidAgeInputException e3)
             {
-                Console.WriteLine(e2.Message);
+                Console.WriteLine(e3.Message);
             }
             finally
             {
-                Console.WriteLine("Block Executed\n");
+                Console.WriteLine("Task 3 Executed \n");
+            }
+        }
+        /// <summary>
+        /// A New Exception to Throw, and catch by AppDomain Unhandled Excetion Handler with StackTrace
+        /// </summary>
+        public void KeyNotFoundException()
+        {  
+            Console.WriteLine("Task 4,5 - KeyNotFoundException");
+            Dictionary<int, string> keys = new Dictionary<int, string>();
+
+            keys.Add(1, "five");
+            try
+            {
+                Console.WriteLine(keys[2]);
+                throw new KeyNotFoundException();
+            }
+            catch (KeyNotFoundException e3)
+            {
+                throw e3;
+            }
+            finally
+            {
+                Console.WriteLine("Unhandled Exception Handled Executed\n");
             }
         }
     }
