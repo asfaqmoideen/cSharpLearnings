f89ff15ae4bbd05c269a5caa09b4cf7b5268ee60
asfaqMoideenSoliton <asfaqmoideen.askar@solitontech.com>
2024-02-01 19:25:54 +0530

    docs: Documentation for Question provided

---
+++ src/Assignemnt10DotNetUnderTheHood/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Assignments
+{
+    /// <summary>
+    /// Initiates the program execution
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// The initialization of the program
+        /// </summary>
+        private static void Main()
+        {
+            Console.WriteLine(".NET Under The Hood");
+
+            Console.WriteLine("Enter ")
+        }
+    }
+}
