fe4fa77102b43b0a42ec594af324cccceef878fa
asfaqMoideenSoliton <asfaqmoideen.askar@solitontech.com>
2024-02-06 19:31:37 +0530

    docs: Documentation Update for GarbageCollection

---
+++ GarbageCollection/Program.cs
@@ -0,0 +1,42 @@
+﻿using GarbageCollection;
+using System.Threading.Channels;
+
+namespace Assignments
+{ 
+    /// <summary>
+    /// Conatains the Main Method
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Inaites the Programor Starting point of the program
+        /// </summary>
+        private static void Main()
+        {
+            //CreateAndDestroyObjects();
+            CreateAndDestroyObjectsWithGCCollector();
+            Console.WriteLine("Programm Running\nPress any key to Exit");
+            Console.ReadKey();
+        }
+
+        private static void CreateAndDestroyObjects()
+        {
+            ProjectCreateAndDetroy projectCreateAndDetroy;
+            for (int i = 0; i < 1000100; i++)
+            {
+                projectCreateAndDetroy = new ProjectCreateAndDetroy(new List<string> (850000000) );
+                projectCreateAndDetroy = null;
+            }
+        }
+        private static void CreateAndDestroyObjectsWithGCCollector()
+        {
+            ProjectCreateAndDetroy projectCreateAndDetroy;
+            for (int i = 0; i < 1000100; i++)
+            {
+                GC.Collect();
+                projectCreateAndDetroy = new ProjectCreateAndDetroy(new List<string>(850000000));
+                projectCreateAndDetroy = null;
+            }
+        }
+    }
+}
---
+++ GarbageCollection/ProjectCreateAndDetroy.cs
@@ -0,0 +1,25 @@
+﻿namespace GarbageCollection
+{
+    /// <summary>
+    /// Class to store Property
+    /// </summary>
+    public class ProjectCreateAndDetroy
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProjectCreateAndDetroy"/> class.
+        /// </summary>
+        /// <param name="projects">list of strings</param>
+        public ProjectCreateAndDetroy(List<string> projects)
+        {
+            Projects = projects;
+        }
+
+        /// <summary>
+        /// projects list
+        /// </summary>
+        /// <value>
+        /// List of project
+        /// </value>
+        public List<string> Projects { get; set; }
+    }
+}
