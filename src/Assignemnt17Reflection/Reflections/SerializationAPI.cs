82a5858ac71901615d16ec6cba8f195bd30c4174
asfaqMoideenSoliton <asfaqmoideen.askar@solitontech.com>
2024-02-23 15:04:01 +0530

    fix: Serialization class created

---
+++ src/Assignemnt17Reflection/Reflections/SerializationAPI.cs
@@ -0,0 +1,15 @@
+﻿namespace Reflections
+{
+    /// <summary>
+    /// Serializes the given content to serializedformat.
+    /// </summary>
+    public class SerializationAPI
+    {
+        /// <summary>
+        /// Serializes the given data to serializedforma
+        /// </summary>
+        public void SerializeData()
+        {
+        }
+    }
+}
